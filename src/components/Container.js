c3d90492cffa24af51a8cff1b2ef2ec2c730eb35
Regina Musovirova <musovirova@yahoo.com>
2020-06-03 11:51:42 -0400

    create table

---
+++ src/components/Container.js
@@ -0,0 +1,12 @@
+import API from "../utils/API";
+import React, { Component } from "react"
+import SearchBox from "./SearchBox"
+// import SearchBox from "./SearchBox"
+import TableData from "./TableData"
+import "./style.css";
+
+class Container extends Component {}
+
+
+
+export default Container
